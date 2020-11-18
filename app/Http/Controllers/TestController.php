b4d02a3bb942123dbbe7292a04e955e4310faaa4
lijikai521 <72546455+lijikai521@users.noreply.github.com>
2020-11-18 19:25:05 +0800

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -25,4 +25,23 @@ class TestController extends Controller
     		echo __METHOD__;
     	}
     }
+    
+    public function AccessToken()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        
+        if( $tmpStr == $signature ){
+            echo $_GET('echostr');
+        }else{
+            echo "111";
+        }
+    }
   }
